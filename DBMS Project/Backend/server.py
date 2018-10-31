1325ee12dba2356aa31eda3dafb6aaef73aacb47
Nishant Rodrigues <nishnash54@gmail.com>
2018-11-01 00:54:47 +0530

    Shipping details route

--- DBMS Project/Backend/server.py
+++ DBMS Project/Backend/server.py
@@ -17,7 +17,6 @@ from pymongo import MongoClient
 # Setup client and database
 client = MongoClient(params['dblink'])
 db = client[params['database_name']]
-collection = db[params['collection_name']]
 
 # Create the app and enable CORS
 app = Flask(__name__)
@@ -28,7 +27,7 @@ CORS(app)
 @app.route('/products')
 def products():
 
-    cursor = collection.find({})
+    cursor = db.products.find({})
     response = []
     for doc in cursor:
         doc.pop('_id', 0)
@@ -37,6 +36,52 @@ def products():
     # Return json response
     response = json.dumps(response)
     return response
+
+# http://localhost:5000/register
+@app.route('/register')
+def register():
+
+    reg = {
+        'name': request.args.get('name'),
+        'email': request.args.get('email'),
+        'password': request.args.get('pass')
+    }
+    print(reg)
+
+    db.login.insert_one(reg)
+    return 'Good'
+
+# http://localhost:5000/login
+@app.route('/login')
+def login():
+
+    uname = request.args.get('email')
+    password = request.args.get('pass')
+
+    val = db.login.find_one({'email': uname})
+
+    if val['password'] == password:
+        return 'Good'
+    return 'Bad'
+
+# http://localhost:5000/shipping
+@app.route('/shipping')
+def track():
+
+    reg = {
+        'name': request.args.get('name'),
+        'email': request.args.get('email'),
+        'contact': request.args.get('contact'),
+        'address': request.args.get('address'),
+        'zip': request.args.get('zip')
+    }
+
+    try:
+        db.shipping.insert_one(reg)
+        return 'Good'
+    except:
+        return 'Bad'
+
 
 if __name__ == '__main__':
     app.run()
